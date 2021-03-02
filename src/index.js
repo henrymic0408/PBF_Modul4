a17852bab9092472ebc22a2638db537673954ecc
henrymic0408 <henrymic0408@gmail.com>
2021-03-02 23:28:56 +0700

    'PBF_Modul4'

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import BlogPost from "./container/BlogPost/BlogPost";
+
+ReactDOM.render(<BlogPost />, document.getElementById('content'));
+
+serviceWorker.unregister();
